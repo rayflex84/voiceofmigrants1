acc2986f86b98e3fb14a082d76529d3801bd3fa5
rayflex84 <45134226+rayflex84@users.noreply.github.com>
2020-04-07 10:55:22 +0100

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,47 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you require will output into a single css file (app.css in this case)
+import '../css/app.css';
+import '../css/aos.css';
+import '../css/bootstrap-datepicker.css';
+import '../css/bootstrap.min.css';
+import '../css/fancybox.min.css';
+import '../css/jquery-ui.css';
+import '../css/jquery.scrollbar.css';
+import '../css/magnific-popup.css';
+import '../css/mediaelementplayer.css';
+import '../css/owl.carousel.min.css';
+import '../css/owl.theme.default.min.css';
+import '../css/style.css';
+import '../css/swiper.min.css';
+import '../fonts/icomoon/style.css';
+import '../fonts/flaticon/font/flaticon.css';
+
+import '../js/main.js';
+import '../js/aos.js';
+import '../js/bootstrap-datepicker.min.js';
+import '../js/bootstrap.min.js';
+import '../js/jquery-3.3.1.min.js';
+import '../js/jquery-ui.js';
+import '../js/jquery.scrollbar.js';
+import '../js/jquery-migrate-3.0.1.min.js';
+import '../js/jquery.countdown.min.js';
+import '../js/jquery.fancybox.min.js';
+import '../js/jquery.magnific-popup.min.js';
+import '../js/mediaelement-and-player.min.js';
+import '../js/owl.carousel.min.js';
+import '../js/jquery.stellar.min.js';
+import '../js/popper.min.js';
+import '../js/slick.min.js';
+import '../js/swiper.min.js';
+import '../js/typed.js';
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// const $ = require('jquery');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
